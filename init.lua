eb750bd15ff5810ba313b966c5ff7f94ae6e9554
Megaf <mmegaf@gmail.com>
2014-06-11 01:37:20 -0300

    Changes to be committed: 	new file:   cobble.lua 	new file:   depends.txt 	new file:   dropbox.lua 	new file:   init.lua 	new file:   secret.lua 	new file:   shared.lua 	new file:   textures/chests.0gb.us_dropbox_front.png 	new file:   textures/chests.0gb.us_dropbox_right.png 	new file:   textures/chests.0gb.us_secret_front.png 	new file:   textures/chests.0gb.us_shared_front.png 	new file:   textures/chests.0gb.us_wifi_front_animated.png 	new file:   textures/chests.0gb.us_wifi_side.png 	new file:   textures/chests.0gb.us_wifi_top.png 	new file:   wifi.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+dofile(minetest.get_modpath("more_chests").."/cobble.lua")
+dofile(minetest.get_modpath("more_chests").."/dropbox.lua")
+dofile(minetest.get_modpath("more_chests").."/secret.lua")
+dofile(minetest.get_modpath("more_chests").."/shared.lua")
+dofile(minetest.get_modpath("more_chests").."/wifi.lua")
+
