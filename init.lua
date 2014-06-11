1884d17f7d32492c0fa386988d60f24cdd555ff6
Megaf <mmegaf@gmail.com>
2014-06-11 13:23:06 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ dofile(minetest.get_modpath("more_chests").."/dropbox.lua")
 dofile(minetest.get_modpath("more_chests").."/secret.lua")
 dofile(minetest.get_modpath("more_chests").."/shared.lua")
 dofile(minetest.get_modpath("more_chests").."/wifi.lua")
-dofile(minetest.get_modpath("mese_chest").."/aliases.lua")
+dofile(minetest.get_modpath("more_chests").."/aliases.lua")
 
