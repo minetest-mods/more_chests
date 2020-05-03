03aee4584f53e42d5eff5c0d370a51ede67b1e2a
k <pnakml@gmail.com>
2020-05-03 16:07:16 +0200

    Add Toolbox models

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ dofile(minetest.get_modpath("more_chests").."/models/dropbox.lua")
 dofile(minetest.get_modpath("more_chests").."/models/fridge.lua")
 dofile(minetest.get_modpath("more_chests").."/models/secret.lua")
 dofile(minetest.get_modpath("more_chests").."/models/shared.lua")
+dofile(minetest.get_modpath("more_chests").."/models/toolbox.lua")
 dofile(minetest.get_modpath("more_chests").."/models/wifi.lua")
 
 dofile(minetest.get_modpath("more_chests").."/utils/aliases.lua")
