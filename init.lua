74b3c5a25dff5e4cf306e7eb93e4ed831a4d29eb
nxet <pnakml@gmail.com>
2020-10-08 11:17:51 +0200

    Mod rewrite and new models (#21)
    * Aliases moved to utils folder
    
    * Method to generate two different formspec layouts, big and small
    
    * Log Actions moved to separate module
    
    * Added method to generate a chest definition
    
    * Rewritten models
    
    * Add Fridge model
    
    * Add Toolbox models
    
    * "Mod loaded" message; Updated localization template
    
    * Add Italian localization
    
    * Fridge now has both normal and big (2 blocks) models
    
    * Fixed mixed indentation
    
    * Rewritten README; improved IT and FR (thanks to @louisroyer) localizations.

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
-dofile(minetest.get_modpath("more_chests").."/cobble.lua")
-dofile(minetest.get_modpath("more_chests").."/dropbox.lua")
-dofile(minetest.get_modpath("more_chests").."/secret.lua")
-dofile(minetest.get_modpath("more_chests").."/shared.lua")
-dofile(minetest.get_modpath("more_chests").."/wifi.lua")
-dofile(minetest.get_modpath("more_chests").."/aliases.lua")
+dofile(minetest.get_modpath("more_chests").."/models/cobble.lua")
+dofile(minetest.get_modpath("more_chests").."/models/dropbox.lua")
+dofile(minetest.get_modpath("more_chests").."/models/fridge.lua")
+dofile(minetest.get_modpath("more_chests").."/models/secret.lua")
+dofile(minetest.get_modpath("more_chests").."/models/shared.lua")
+dofile(minetest.get_modpath("more_chests").."/models/toolbox.lua")
+dofile(minetest.get_modpath("more_chests").."/models/wifi.lua")
 
+dofile(minetest.get_modpath("more_chests").."/utils/aliases.lua")
+
+print ("[MOD] more_chests loaded")
